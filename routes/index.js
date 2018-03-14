8705978d5313d6ea2b4a513bc88bbba28e050e5b
avatardemy <mademigoke@yahoo.com>
2018-03-14 22:50:34 +0100

    commit akoko

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//Show me hello world
+// router.get('/hello', function(res, req, next){
+//   res.render('hello', {title: 'Hello!'});
+// });
+
+module.exports = router;
